7c56d049d3d9c9e6a33d2d21d0df72854986f544
PrinceMalviyaDev <pm306616@gmail.com>
2026-05-21 00:41:07 +0530

    revise basic commands

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
-const app = express();   // created the server and saved into the app variable
+const app = express();   // created a server instance and saved into the app variable
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+});
+
+app.get("/about", (req, res) => {
+    res.send("About Page")
+})
 
 app.listen(3000);  // start the server
